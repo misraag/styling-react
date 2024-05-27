b6732285f9dc6370753ae71499cbdc907792472c
misraag <jaiofficial50@gmail.com>
2024-05-27 20:13:11 +0530

    finalized with tailwind css

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,18 +1,6 @@
 import { useState } from "react";
-import { styled } from "styled-components";
 import Button from "./Buttons";
-import CustomInput from "./Input";
-
-const CustomComponent = styled.div`
-  width: 100%;
-  max-width: 28rem;
-  padding: 2rem;
-  margin: 0 auto;
-  border-radius: 0.5rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  background: linear-gradient(180deg, #474232 0%, #28271c 100%);
-  color: white;
-`;
+import Input from "./Input";
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -35,15 +23,15 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <CustomComponent id="auth-inputs">
-      <div className="controls">
-        <CustomInput
+    <div id="auth-inputs" className="w-full mx-auto max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
+        <Input
           label="Email"
           type="email"
           invalid={emailNotValid}
           onChange={(event) => handleInputChange("email", event.target.value)}
         />
-        <CustomInput
+        <Input
           label="Password"
           type="password"
           invalid={passwordNotValid}
@@ -52,14 +40,14 @@ export default function AuthInputs() {
           }
         />
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className="button" onClick={handleLogin}>
+        <Button  onClick={handleLogin}>
           Sign In
         </Button>
       </div>
-    </CustomComponent>
+    </div>
   );
 }
