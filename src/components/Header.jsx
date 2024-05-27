9f9020e2d0c378755083b2634a111787b675667b
misraag <jaiofficial50@gmail.com>
2024-05-27 18:24:13 +0530

    added css components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import logo from '../assets/logo.png';
+import classes from './Header.module.css'
 
 export default function Header() {
   return (
-    <header>
+    <header className={classes.header}>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
